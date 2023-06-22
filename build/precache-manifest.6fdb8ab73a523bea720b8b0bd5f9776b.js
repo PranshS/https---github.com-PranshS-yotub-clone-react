100217b756157fa3210fc9724d244bf8d419b3a1
PranshS <sharmapranshu422@gmail.com>
2023-06-22 19:40:55 +0530

    build commit

---
+++ build/precache-manifest.6fdb8ab73a523bea720b8b0bd5f9776b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23df79ce5ad39ec49331",
+    "url": "/static/css/main.8d05512c.chunk.css"
+  },
+  {
+    "revision": "23df79ce5ad39ec49331",
+    "url": "/static/js/main.23df79ce.chunk.js"
+  },
+  {
+    "revision": "91661a90d78dfd2d3e6d",
+    "url": "/static/js/1.91661a90.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "680bf52f4e0e245f8506d38db2e3b749",
+    "url": "/index.html"
+  }
+];
